3102fbae0b24add2d18145a41ab77e050d8a0ceb
lopingsuee <umaygans03@gmail.com>
2024-12-26 21:16:43 +0700

    add pembukuan page

--- Pembukuan Kynan/ADMIN/frm_mainAdmin.Designer.vb
+++ Pembukuan Kynan/ADMIN/frm_mainAdmin.Designer.vb
@@ -22,9 +22,171 @@ Partial Class frm_mainAdmin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "frm_mainAdmin"
+        Label1 = New Label()
+        Panel1 = New Panel()
+        Label3 = New Label()
+        TableLayoutPanel1 = New TableLayoutPanel()
+        Button5 = New Button()
+        Button4 = New Button()
+        Button3 = New Button()
+        Button2 = New Button()
+        Button1 = New Button()
+        Panel2 = New Panel()
+        Panel1.SuspendLayout()
+        TableLayoutPanel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = SystemColors.Highlight
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(146, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "PEMBUKUAN"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(TableLayoutPanel1)
+        Panel1.Controls.Add(Label1)
+        Panel1.Dock = DockStyle.Top
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(941, 79)
+        Panel1.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.Black
+        Label3.Location = New Point(28, 39)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(108, 21)
+        Label3.TabIndex = 4
+        Label3.Text = "Dapur Kynan"
+        ' 
+        ' TableLayoutPanel1
+        ' 
+        TableLayoutPanel1.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        TableLayoutPanel1.ColumnCount = 5
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 20F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 20F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 20F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 20F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 20F))
+        TableLayoutPanel1.Controls.Add(Button5, 4, 0)
+        TableLayoutPanel1.Controls.Add(Button4, 3, 0)
+        TableLayoutPanel1.Controls.Add(Button3, 2, 0)
+        TableLayoutPanel1.Controls.Add(Button2, 1, 0)
+        TableLayoutPanel1.Controls.Add(Button1, 0, 0)
+        TableLayoutPanel1.Location = New Point(429, 3)
+        TableLayoutPanel1.Name = "TableLayoutPanel1"
+        TableLayoutPanel1.RowCount = 1
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 100F))
+        TableLayoutPanel1.Size = New Size(509, 69)
+        TableLayoutPanel1.TabIndex = 2
+        ' 
+        ' Button5
+        ' 
+        Button5.FlatAppearance.BorderSize = 0
+        Button5.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack
+        Button5.FlatStyle = FlatStyle.Flat
+        Button5.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button5.Location = New Point(407, 3)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(95, 63)
+        Button5.TabIndex = 6
+        Button5.Text = "Logout"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.FlatAppearance.BorderSize = 0
+        Button4.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack
+        Button4.FlatStyle = FlatStyle.Flat
+        Button4.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(306, 3)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(95, 63)
+        Button4.TabIndex = 5
+        Button4.Text = "Report"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.FlatAppearance.BorderSize = 0
+        Button3.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(205, 3)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(95, 63)
+        Button3.TabIndex = 4
+        Button3.Text = "Manage Users"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.FlatAppearance.BorderSize = 0
+        Button2.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(104, 3)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(95, 63)
+        Button2.TabIndex = 3
+        Button2.Text = "Manage Stock"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(3, 3)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(95, 63)
+        Button1.TabIndex = 2
+        Button1.Text = "Manage Product"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.WhiteSmoke
+        Panel2.Dock = DockStyle.Bottom
+        Panel2.Location = New Point(0, 423)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(941, 65)
+        Panel2.TabIndex = 2
+        ' 
+        ' frm_mainAdmin
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(941, 488)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        Name = "frm_mainAdmin"
+        Text = "Pembukuan Kynan"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        TableLayoutPanel1.ResumeLayout(False)
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel2 As Panel
 End Class
